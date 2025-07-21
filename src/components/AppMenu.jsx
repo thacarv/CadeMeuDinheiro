b819122e015f297b0e7e8beca3e15a784129424d
Monosune <thiagoassisdecarvalho@gmail.com>
2025-07-21 11:09:03 -0300

    0.2.0

--- src/components/AppMenu.jsx
+++ src/components/AppMenu.jsx
@@ -1,7 +1,23 @@
+import { ArrowRightLeft, TableProperties, ChartLine } from "lucide-react";
+
 function AppMenu() {
+  const menuList = [
+    <ArrowRightLeft size={40} />,
+    <TableProperties size={40} />,
+    <ChartLine size={40} />,
+  ];
+
   return (
     <>
-      <div className="div-app-theme h-[10vh] my-15 "></div>
+      <div className="div-app-theme flex justify-around items-center h-[8vh] my-15 ">
+        {menuList.map((item) => {
+          return (
+            <div className="h-[40px] w-[40px] text-[#A7A7A7] size-4">
+              {item}
+            </div>
+          );
+        })}
+      </div>
     </>
   );
 }
